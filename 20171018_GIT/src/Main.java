2ecd5423c356b2dfb577497a9aeb503e1ef4d501
pc-10 <pc-10@pc-10-PC>
2017-10-18 14:43:30 +0900

    ssss출력문까지

--- 20171018_GIT/src/Main.java
+++ 20171018_GIT/src/Main.java
@@ -2,11 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-
-		
 		
 		System.out.println("¼öÁ¤!");
-		
+		System.out.println("print A");
+		System.out.println("print B");
+		System.out.println("ssssss");
 		
 		
 		
